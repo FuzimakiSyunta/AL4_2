ed55dca10e04e3267ea2bec9cec48fbd11c8788d
FuzimakiSyunta <115062898+FuzimakiSyunta@users.noreply.github.com>
2024-02-09 18:02:28 +0900

    完成

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -104,13 +104,13 @@ void Player::Update()
 	    worldTransform_.translation_.x, worldTransform_.translation_.y,
 	    worldTransform_.translation_.z};
 
-	// 画面の座標を表示
-	ImGui::Begin("Player");
-	ImGui::SliderFloat3("playerRot", playerRot, -28.0f, 28.0f);
-	ImGui::SliderFloat3("playerPos", playerPos, -28.0f, 28.0f);
-	ImGui::Text("%d\n", behaviorRequest_);
-	ImGui::Text("%d\n", Count_);
-	ImGui::End();
+	//// 画面の座標を表示
+	//ImGui::Begin("Player");
+	//ImGui::SliderFloat3("playerRot", playerRot, -28.0f, 28.0f);
+	//ImGui::SliderFloat3("playerPos", playerPos, -28.0f, 28.0f);
+	//ImGui::Text("%d\n", behaviorRequest_);
+	//ImGui::Text("%d\n", Count_);
+	//ImGui::End();
 
 	worldTransform_.translation_.x = playerPos[0];
 	worldTransform_.translation_.y = playerPos[1];
